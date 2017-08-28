e72bed534d3593b1dbabcd0e46b3c1b29d4c6386
libw <3180234769@qq.com>
2017-08-28 11:08:48 +0800

    xiugai6

--- src/components/partnerReg/input/index.js
+++ src/components/partnerReg/input/index.js
@@ -5,56 +5,41 @@ class FormItem extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            isFocus:false,
-            sta:true,
-            frc:{
-                color:"#dfdce4",
-                borderColor:"#dfdce4"
-            }
+            cla:"default"
         }
     }
-
     render(){
         return(
             <div className={style.inp} style={this.props.st}>
-                <span style={this.state.frc}>
+                <span className={style[this.state.cla]}>
                     {this.props.tip}
                 </span>
-                <input type="text" placeholder={this.props.pla} onFocus={this.onfocus.bind(this)} onBlur={this.onbulr.bind(this)}  name={this.props.nn} style={this.state.frc}/>
+                <input type="text" placeholder={this.props.pla} onFocus={this.onfocus.bind(this)} onBlur={this.onbulr.bind(this)}  name={this.props.nn} className={style[this.state.cla]}/>
             </div>
         )
     }
     //失去焦点
     onbulr (){
         this.setState({
-            isFocus:false
-        });
-        if(this.state.sta){
-            this.setState({
-                frc:{
-                    color:"#dfdce4",
-                    borderColor:"#dfdce4"
-                }
-            })
+            cla:"error"
+        },()=>{
+            if(this.state.cla=="error"){
 
-        }else {
-            this.setState({
-                frc:{
-                    color:"#f59294",
-                    borderColor:"#f59294"
-                }
-            })
-        }
+
+            }else {
+
+                this.setState({
+                    cla:"default"
+                })
+            }
+        })
+
     }
 
     //获取焦点
     onfocus(){
         this.setState({
-            isFocus:true,
-            frc:{
-                color:"#5262ff",
-                borderColor:"#5262ff"
-            }
+            cla:"isfocus"
         })
     }
 

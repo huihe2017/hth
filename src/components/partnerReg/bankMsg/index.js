5a7f5ea7967597e124dc98b360edc8fbe8d513d6
libw <3180234769@qq.com>
2017-08-29 10:33:38 +0800

    xiugai13

--- src/components/partnerReg/bankMsg/index.js
+++ src/components/partnerReg/bankMsg/index.js
@@ -1,21 +1,47 @@
 import React from 'react';
 import style from  "./index.css"
+import Title from "../title/index";
+import Webfile from "../webFile/index";
+import Input from "../input/index";
+import Tipdown from "../tipDown/index";
+import Selectarea from "../selectArea/index";
 
+class Bankmsg extends React.Component{
 
-class xxxxx extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-
-        }
     render(){
-
+        let putword1=["点击上传银行卡正面","点击上传银行卡反面"];
+        let tipword1=[
+            "1.文件为数码照片，请勿进行美化和修改，以免申请失败",
+            "2.上传文件格式支持png，jpg和bmp",
+            "3.文件大小不超过3MB，文件尺寸最小为200px*150px"
+        ];
+        let bank1=[
+            {
+                key:1,
+                value:"中国人民银行"
+            },
+            {
+                key:2,
+                value:"中国建设银行"
+            },
+            {
+                key:3,
+                value:"中国农业银行"
+            }
+        ];
         return(
-            <div className={style.wrap} ></div>
+            <div className={style.personal} >
+                <Title content={"/银行信息【必填】"}/>
+                <div className={style.perimport}>
+                    <Input st={'100%'} pla={"结算户名"} tip={"文案待定"}/>
+                    <Input st={'100%'} pla={"结算卡号"} tip={"文案待定"}/>
+                    <Tipdown show={"请选择银行"} data={bank1} ww={'100%'} tip={"文案待定"}/>
+                    <Selectarea/>
+                </div>
+                <Webfile title={"上传银行卡照片"} putword={putword1} tipword={tipword1}/>
+            </div>
         )
-
     }
-
 }
 
-export default xxxxx;
+export default Bankmsg;
--- src/components/partnerReg/companyMsg/index.js
+++ src/components/partnerReg/companyMsg/index.js
@@ -1,21 +1,51 @@
-import React from 'react'
-import style from  "./index.css"
+import React from 'react';
+import style from  "./index.css";
+import Title from "../title/index";
+import Webfile from "../webFile/index";
+import Input from "../input/index";
 
 
-class PartnerReg extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-
-        }
+class Company extends React.Component{
     render(){
+        let data1=[{
+            value:["中国大陆","+86"],
+            key:1
+        }, {
+            value:["中国香港","+886"],
+            key:2,
+            checked:true
+        }, {
+            value:["中国台湾","+853"],
+            key:3,
 
+        }];
+        let putword1=["点击上传营业执照"];
+        let tipword1=[
+            "1.文件为数码照片，请勿进行美化和修改，以免申请失败",
+            "2.上传文件格式支持png，jpg和bmp",
+            "3.文件大小不超过3MB，文件尺寸最小为200px*150px"
+        ];
         return(
-            <div className={style.wrap} >253</div>
+            <div className={style.personal} >
+                <Title content={"/公司信息"}/>
+                <div className={style.perimport}>
+                    <Input st={'100%'} pla={"公司名"} tip={"文案待定"}/>
+                    <Input st={'100%'} pla={"公司地址"} tip={"文案待定"}/>
+                    <Input st={'100%'} pla={"统一公司信用代码"} tip={"文案待定"}/>
+
+                    <Input st={'100%'} pla={"组织机构代码"} tip={"文案待定"}/>
+                    <Input st={'100%'} pla={"法人"} tip={"文案待定"}/>
+                </div>
+                <div className={style.upfile}>
+                    <Webfile title={"上传营业执照照片"} putword={putword1} tipword={tipword1}/>
+                </div>
+
+
+            </div>
         )
 
     }
 
 }
 
-export default PartnerReg;
+export default Company;

c794bbccd79a92ea5fb5de722826368f2c74352f
libw <3180234769@qq.com>
2017-08-29 14:39:34 +0800

    xiugai17

--- src/components/ib/titValueBox/index.js
+++ src/components/ib/titValueBox/index.js
@@ -5,12 +5,25 @@ import style from "./index.css"
 class TitValueBox extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+
+        }
     }
 
     render() {
         return (
-            <div className={style.wrap}></div>
+            <div className={style.titbox}>
+                <span className={style.title}>
+                    {this.props.tit+" :"}
+                </span>
+                <div className={style.titval}>
+                    {
+                        React.Children.map(this.props.children, function (child) {
+                            return <li>{child}</li>;
+                        })
+                    }
+                </div>
+            </div>
         )
     }
 }

f2c6094d92a42fcd1adb5254093313cab9e9fa4f
libw <3180234769@qq.com>
2017-09-01 10:32:27 +0800

    xiugai35

--- src/components/ib/index.js
+++ src/components/ib/index.js
@@ -6,7 +6,8 @@ import AccountLink from './AccountLink'
 import ChildLink from './ChildLink'
 import ReportQuery from './ReportQuery'
 import ApplyCommission from './ApplyCommission'
-import AccountOverview from './accountOverview'
+import AccountOverview from './accountOverview';
+import TabControl from './tabControl'
 
 class Ib extends React.Component {
     constructor(props) {
@@ -15,10 +16,68 @@ class Ib extends React.Component {
     }
 
     render() {
-
+        let navdata=[
+            {
+                icon:"user-o",
+                value:"IB账号",
+                key:0
+            },
+            {
+                icon:"sign-out",
+                value:"申请出佣",
+                key:1
+            },
+            {
+                icon:"pencil-square-o",
+                value:"开户链接",
+                key:2
+            },
+            {
+                icon:"list-alt",
+                value:"子代理链接",
+                key:3
+            },
+            {
+                icon:"sliders",
+                value:"报表查询",
+                key:4
+            },
+            {
+                icon:"address-book-o",
+                value:"用户分组",
+                key:5
+            }
+        ];
+        let data1=[
+            {
+                key:1,
+                value:"用户资料"
+            },
+            {
+                key:2,
+                value:"持仓记录"
+            },
+            {
+                key:3,
+                value:"平仓记录"
+            },
+            {
+                key:4,
+                value:"出入金记录"
+            },
+            {
+                key:5,
+                value:"出佣记录"
+            },
+            {
+                key:6,
+                value:"返佣记录"
+            }
+        ]
         return (
             <div className={style.wrap}>
-                <TotalNav/>
+                <TotalNav data={this.props.navdata?this.props.navdata:navdata}/>
+                <TabControl data={data1}/>
                 <Router>
                     <div>
                         <Route path="/ib" component={AccountOverview}/>
@@ -36,3 +95,17 @@ class Ib extends React.Component {
 }
 
 export default Ib;
+
+/*
+*
+* {
+                    this.props.data.map((v,i)=>{
+
+                        return (
+                            <div  onClick={this.handleClick.bind(this,v.key)} className={style.nav+" "+((this.state.index == v.key)?style.active:'')}>
+                                 {v.value}
+                            </div>
+                            )
+                    })
+                }
+* */
--- src/components/ib/tabControl/index.js
+++ src/components/ib/tabControl/index.js
@@ -3,26 +3,29 @@ import style from  "./index.css";
 
 
 class TabControl extends React.Component{
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            index: 1
+        }
+    }
+    handleClick(i) {
+        this.setState({
+            index: i
+        })
+    }
     render(){
+        console.log(this.state.index)
         return(
             <div className={style.reportquery}>
 
                 {
                     this.props.data.map((v,i)=>{
-                        if(v.checked){
-                            return (
-                                <div className={style.naved}>
-                                    {v.value}
-                                </div>
+                        return (
+                            <div  onClick={this.handleClick.bind(this,v.key)} className={style.nav+" "+((this.state.index == v.key)?style.active:'')}>
+                                 {v.value}
+                            </div>
                             )
-                        }else {
-                            return (
-                                <div className={style.nav}>
-                                    {v.value}
-                                </div>
-                            )
-                        }
                     })
                 }
 

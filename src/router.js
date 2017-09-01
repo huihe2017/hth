be6d487507630fb2b2ba920264ecd824ec9349f9
yuwenbin1988 <460268358@qq.com>
2017-09-01 11:45:18 +0800

    路由优化

--- src/router.js
+++ src/router.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {HashRouter as Router, Route, Switch} from 'react-router-dom'
 
 import './common.css'
 import App from './components/app'

b9de38a378713aea1c81c6e2fea8911a968b321c
yuwenbin1988 <460268358@qq.com>
2017-09-05 09:40:12 +0800

    新增个人中心头部

---
+++ src/components/userCenterHead/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from "./index.css"
+import UserCenterHeadView from './userCenterHeadView'
+
+
+class UserCenterHead extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            portrait: 'http://xxxxx.jpg',
+            userName: '嘎嘎',
+            phone: ['86', '13725565878'],
+            openingTime: 364747474745,
+            floating: 44,
+            worth: 33,
+            balance: 444
+        }
+    }
+
+    binding() {
+        alert(10)
+    }
+
+    render() {
+
+        return (
+            <UserCenterHeadView handle={binding} {...this.state} />
+        )
+
+    }
+}
+
+export default UserCenterHead;

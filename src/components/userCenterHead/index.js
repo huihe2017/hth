cb188a29dd43998fd37f1d3f83a48cfe8c1bacef
libw <3180234769@qq.com>
2017-09-05 11:42:57 +0800

    xiugai43

--- src/components/userCenterHead/index.js
+++ src/components/userCenterHead/index.js
@@ -7,7 +7,7 @@ class UserCenterHead extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            portrait: 'http://xxxxx.jpg',
+            portrait: 'http://img1.tgbusdata.cn/v2/thumb/jpg/NkRCMiw2NDAsMTAwLDQsMywxLC0xLDAscms1MA==/u/wow.tgbus.com/UploadFiles_2396/201605/20160530094443812.jpg',
             userName: '嘎嘎',
             phone: ['86', '13725565878'],
             openingTime: 364747474745,
--- src/components/userCenterHead/userCenterHeadView/index.js
+++ src/components/userCenterHead/userCenterHeadView/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from  "./index.css"
 
 
-class xxxxx extends React.Component{
+class userCenterHeadView extends React.Component{
     constructor(props){
         super(props);
         this.state = {
@@ -10,11 +10,73 @@ class xxxxx extends React.Component{
         }
 	}
 	render(){
-
+            let imgurl= ""
 			return(
-				<div className={style.wrap} ></div>
+				<div className={style.userCenterHeadView}>
+                    <div className={style.userchvheader}>
+                        <div className={style.avatar}>
+                            <img src={this.props.portrait?this.props.portrait:require('../../partnerReg/images/avatar-default.png')} alt=""/>
+                        </div>
+                        <div className={style.userhc}>
+                            <div className={style.userhch}>
+                                <div className={style.username}>
+                                    {this.props.userName}
+                                </div>
+                                <div className={style.userphone}>
+                                    {
+                                        this.props.phone.map((v)=>{
+                                            return (
+                                                <span>{v}&nbsp;</span>
+                                            )
+                                        })
+                                    }
+                                </div>
+                            </div>
+                            <div className={style.userhcc}>
+                                <div>
+                                    出金绑定银行卡：
+                                    <a href="javascript:void (0);">去绑定</a>
+                                <div/>
+                                <div>
+                                    出户时间：{this.props.openingTime}
+
+                                </div>
+
+
+
+                                </div>
+                            </div>
+				        </div>
+				    </div>
+                    <div className={style.userchvfooter}>
+                        <div className={style.floating}>
+                            <div className={style.money}>
+                                ${this.props.floating}
+                            </div>
+                            <div className={style.top}>
+                                浮动盈亏
+                            </div>
+                        </div>
+                        <div className={style.worth}>
+                            <div className={style.money}>
+                                ${this.props.worth}
+                            </div>
+                            <div className={style.top}>
+                                净值
+                            </div>
+                        </div>
+                        <div className={style.balance}>
+                            <div className={style.money}>
+                                ${this.props.balance}
+                            </div>
+                            <div className={style.top}>
+                                余额
+                            </div>
+                        </div>
+                    </div>
+                </div>
 			)
 
 		}
 }
-export default xxxxx;
+export default userCenterHeadView;

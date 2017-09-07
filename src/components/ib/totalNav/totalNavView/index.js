c905dc42d51f8aa87dfd12658805595d0989b8fb
libw <3180234769@qq.com>
2017-09-07 14:15:08 +0800

    xiugai54

--- src/components/ib/totalNav/totalNavView/index.js
+++ src/components/ib/totalNav/totalNavView/index.js
@@ -5,14 +5,16 @@ import {Link} from 'react-router-dom'
 class TotalNav extends React.Component {
 
     render() {
-
+        let w={
+            width:100/this.props.data.length+'%'
+        };
         return (
             <div className={style.wrap}>
                 <ul>
                     {this.props.data.map((v) => {//alert(v.path)
                         return (
                             <Link to={v.path} >
-                                <li className={style.nav + " " + (( v.checked) ? style.active : '')}>
+                                <li className={style.nav + " " + (( v.checked) ? style.active : '')} style={w}>
                                     <span className={"fa fa-" + v.icon}></span>
                                     {v.value}
                                 </li>

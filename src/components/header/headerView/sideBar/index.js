046bfc72be3da8cade26ee813bf249cb01f1d39a
yuwenbin1988 <460268358@qq.com>
2017-09-11 11:36:29 +0800

    sidebar组件优化

--- src/components/header/headerView/sideBar/index.js
+++ src/components/header/headerView/sideBar/index.js
@@ -2,16 +2,25 @@ import React, {Component} from 'react'
 import Layer from '../../../renderLayer/index'
 import {Link} from 'react-router-dom'
 import style from './sideBar.css'
+import {hashHistory} from 'react-router';
+
 
 class SideBar extends Component {
 
-    onClickHandle(e){
-        if(localStorage.userName==='null'){
+    onClickHandle(e) {
+        if (localStorage.userName === 'null') {
             e.preventDefault()
             this.props.userCenterClick(true)
         }
 
     }
+
+    checkedForPath(sidePath) {
+        if (sidePath === hashHistory.getCurrentLocation().pathname) {//alert(22)
+            return style.checked
+        }
+    }
+
     render() {
         let isHide = this.props.show ? '' : style.hide
 
@@ -19,33 +28,69 @@ class SideBar extends Component {
             <div>
                 <div className={style.sideBar + ' ' + isHide}>
                     <Link to="/">
-                        <div className={style.checked}>首页</div>
+                        <div className={(() => {
+                            return this.checkedForPath('/')
+                        })()}>首页
+                        </div>
                     </Link>
-                    <Link onClick={this.onClickHandle.bind(this)} to="/ProductDeal">
-                        <div>产品交易</div>
+                    <Link to="/ProductDeal">
+                        <div className={(() => {
+                            return this.checkedForPath('/ProductDeal')
+                        })()}>产品交易
+                        </div>
                     </Link>
-                    <Link onClick={this.onClickHandle.bind(this)} to="/DealTerrace">
-                        <div>交易平台</div>
+                    <Link to="/DealTerrace">
+                        <div className={(() => {
+                            return this.checkedForPath('/DealTerrace')
+                        })()}>交易平台
+                        </div>
                     </Link>
-                    <Link onClick={this.onClickHandle.bind(this)} to="/aboutUs">
-                        <div>关于海豚汇</div>
+                    <Link to="/aboutUs">
+                        <div className={(() => {
+                            return this.checkedForPath('/aboutUs')
+                        })()}>关于海豚汇
+                        </div>
                     </Link>
-                    <Link onClick={this.onClickHandle.bind(this)} to="/DolphinSchool">
-                        <div>合伙人计划</div>
+                    <Link to="/DolphinSchool">
+                        <div className={(() => {
+                            return this.checkedForPath('/DolphinSchool')
+                        })()}>合伙人计划
+                        </div>
                     </Link>
-                    <Link onClick={this.onClickHandle.bind(this)} to="/DolphinSchool">
-                        <div>海豚学院</div>
+                    <Link to="/DolphinSchool">
+                        <div className={(() => {
+                            return this.checkedForPath('/DolphinSchool')
+                        })()}>海豚学院
+                        </div>
                     </Link>
                     <Link onClick={this.onClickHandle.bind(this)} to="/userCenter">
-                        <div>个人中心</div>
+                        <div className={(() => {
+                            return this.checkedForPath('/userCenter')
+                        })()}>个人中心
+                        </div>
                     </Link>
-                    <Link to="/ib"><div>代理系统</div></Link>
-                    <Link to="/partnerReg"><div>合伙人注册</div></Link>
-                    <Link to="/partnerLogin"><div>合伙人登录</div></Link>
+                    <Link to="/ib">
+                        <div className={(() => {
+                            return this.checkedForPath('/ib')
+                        })()}>代理系统
+                        </div>
+                    </Link>
+                    <Link to="/partnerReg">
+                        <div className={(() => {
+                            return this.checkedForPath('/partnerReg')
+                        })()}>合伙人注册
+                        </div>
+                    </Link>
+                    <Link to="/partnerLogin">
+                        <div className={(() => {
+                            return this.checkedForPath('/partnerLogin')
+                        })()}>合伙人登录
+                        </div>
+                    </Link>
 
 
                 </div>
-                {this.props.show?<Layer></Layer>:''}
+                {this.props.show ? <Layer></Layer> : ''}
 
             </div>
         )
